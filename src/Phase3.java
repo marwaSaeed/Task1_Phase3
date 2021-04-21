5e745aecea3d74d6ff6261201b35007b443ab673
marwaSaeed <52875604+marwaSaeed@users.noreply.github.com>
2021-04-21 06:39:31 +0530

    Update Phase3.java

--- src/Phase3.java
+++ src/Phase3.java
@@ -13,24 +13,36 @@ import java.util.*;
 import java.util.LinkedList;
 
 class Phase3{
+	/**
+	* <h1>max-flow min-cut algorithm</h1>
+	* This class implements the max-flow min-cut algorithm for task 1  
+	* Displays the flow augmentation path found and its capacity in each step 
+	* Displays the maximum flow of the network, and the corresponding min-cut 
+
+	* <p>
+	* 
+	* 
+	*
+	*/
+	
 	static final int V = 6; // Number of vertices in graph
 	boolean visited[] = new boolean[V];
 	
-	/* Returns true if there is a path from source 's' to
+	/** Returns true if there is a path from source 's' to
 	sink 't' in residual graph. Also fills parent[] to
 	store the path */
 	boolean bfs(int rGraph[][], int s, int t, int parent[])
 	{
-		// Create a visited array and mark all vertices as
-		// not visited
+		/** Create a visited array and mark all vertices as
+		not visited*/
 	
 	
 		for (int i = 0; i < V; ++i) {
 			visited[i] = false;
 	
 		}
-		// Create a queue, enqueue source vertex and mark
-		// source vertex as visited
+		/**Create a queue, enqueue source vertex and mark
+		// source vertex as visited*/
 		LinkedList<Integer> queue = new LinkedList<Integer>();
 		queue.add(s);
 		visited[s] = true;
@@ -47,10 +59,10 @@ class Phase3{
 			
 				if (visited[v] == false && rGraph[u][v] > 0) {
 				
-					// If we find a connection to the sink
+					/**If we find a connection to the sink
 					// node, then there is no point in BFS
 					// anymore We just have to set its parent
-					// and can return true
+					// and can return true*/
 		
 					
 					if (v == t) {
@@ -67,25 +79,25 @@ class Phase3{
 		}
 		
 	
-		// We didn't reach sink in BFS starting from source,
-		// so return false
+		/** We didn't reach sink in BFS starting from source,
+		// so return false*/
 		return false;
 	}
 
-	// Returns tne maximum flow from s to t in the given
-	// graph
+	/** Returns tne maximum flow from s to t in the given
+	 graph*/
 	int fordFulkerson(int graph[][], int s, int t)
 	{
 		int min ;
 		int u, v;
 		int Capcity=0;
-		// Create a residual graph and fill the residual
+		/**Create a residual graph and fill the residual
 		// graph with given capacities in the original graph
 		// as residual capacities in residual graph
 		// Residual graph where rGraph[i][j] indicates
 		// residual capacity of edge from i to j (if there
 		// is an edge. If rGraph[i][j] is 0, then there is
-		// not)
+		// not)*/
 		int rGraph[][] = new int[V][V];
 	
 		for (u = 0; u < V; u++) {
@@ -94,11 +106,11 @@ class Phase3{
 			}
 		
 		}
-		// This array is filled by BFS and to store path
+		/**This array is filled by BFS and to store path*/
 		int parent[] = new int[V];
-		int max_flow = 0; // There is no flow initially
+		int max_flow = 0; /** There is no flow initially
 		// Augment the flow while tere is path from source
-		// to sink
+		// to sink*/
 		while (bfs(rGraph, s, t, parent)) {
 			ArrayList<Integer> AguPath = new ArrayList<Integer>();
 			// Find minimum residual capacity of the edhes
@@ -158,15 +170,15 @@ for(int i = 0 ; i< visited.length;i++) {
 	}
 	
 }
-		// Return the overall flow
+		/** Return the overall flow**/
 		return max_flow;
 	}
 
-	// Driver program to test above functions
+	/** Driver program to test above functions**/
 	public static void main(String[] args)
 		throws java.lang.Exception
 	{
-		// Let us create a graph shown in the above example
+		/** Let us create a graph shown in the above example*/
 		int graph[][] = new int[][] {
 			{ 0, 2, 7, 0, 0, 0 }, { 0, 0, 0, 3, 4, 0 },
 			{ 0, 0, 0, 4, 2, 0 }, { 0, 0, 0, 0, 0, 1 },
